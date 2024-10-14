9784b5aba20b2f24bd36f22573527a7b9d81799e
zmecdae <zmecdae@ericsson.com>
2024-10-14 13:50:53 -0300

    feat: new tag

---
+++ src/assets/js/google-tag-manager.js
@@ -0,0 +1,8 @@
+(function (w, d, s, l, i) {
+    w[l] || []; w[l].push({
+        'gtm.start':
+            new Date().getTime(), event: 'gtag.js'
+    }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+            'https://www.googletagmanager.com/gtag/js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-PKCH3DTG');
